7a611ea50f230baf2e0a13516a1dd9fcfffefc89
Beth <Beth3346@gmail.com>
2021-12-18 11:15:19 -0600

    move updates to demo

--- src/script.js
+++ src/script.js
@@ -1,47 +1,104 @@
 "use-strict";
 
 (() => {
-  const buttons = Array.from(
-    document.getElementsByClassName("js-filter-button")
+  const filters = Array.from(
+    document.getElementsByClassName("js-filter-dropdown")
   );
 
-  console.log({ buttons });
-  buttons.forEach(button => {
-    const parent = button.parentNode;
-    const items = Array.from(parent.querySelectorAll("li"));
+  filters.forEach(filter => {
+    const buttons = Array.from(
+      filter.getElementsByClassName("js-filter-button")
+    );
 
-    items.forEach((item, index) => {
-      item.querySelector("button").addEventListener("keyup", e => {
-        if (e.key === "ArrowDown" && index !== items.length - 1) {
-          const next = items[index + 1];
+    // TODO: add type search feature
+    // TODO: add clear selection button
 
-          if (next) {
-            next.querySelector("button").focus();
+    buttons.forEach(button => {
+      const parent = button.parentNode;
+      const items = Array.from(parent.querySelectorAll("li"));
+
+      items.forEach((item, index) => {
+        const btnItem = item.querySelector("button");
+
+        const setValue = (val, label) => {
+          button.firstChild.textContent = label;
+          button.setAttribute("data-selection", val);
+          filter.classList.remove("active");
+        };
+
+        btnItem.addEventListener("keyup", e => {
+          // e.stopPropagation();
+
+          if (e.key === "ArrowDown" && index !== items.length - 1) {
+            const next = items[index + 1];
+
+            if (next) {
+              next.querySelector("button").focus();
+            }
+          } else if (e.key === "ArrowUp" && index !== 0) {
+            const prev = items[index - 1];
+
+            if (prev) {
+              prev.querySelector("button").focus();
+            }
+          } else if (e.key === "Escape") {
+            parent.classList.remove("active");
           }
-        } else if (e.key === "ArrowUp" && index !== 0) {
-          const prev = items[index - 1];
+        });
 
-          if (prev) {
-            prev.querySelector("button").focus();
-          }
+        btnItem.addEventListener("click", e => {
+          e.stopPropagation();
+          setValue(btnItem.getAttribute("data-value"), btnItem.textContent);
+          button.focus();
+        });
+      });
+
+      button.addEventListener("click", e => {
+        e.stopPropagation();
+        parent.classList.toggle("active");
+        items[0].getElementsByTagName("button")[0].focus();
+      });
+
+      parent.addEventListener("keyup", e => {
+        if (e.key === "Escape") {
+          parent.classList.remove("active");
         }
       });
     });
+  });
+})();
 
-    button.addEventListener("click", () => {
-      parent.classList.toggle("active");
-    });
+(() => {
+  // TODO: build badge list selection filter
+  // - should be able to select multiple items at the same time
+  // - should be able to navigation using arrow keys
+  const selectionFilters = Array.from(
+    document.getElementsByClassName("js-selection-filters")
+  );
 
-    parent.addEventListener("keyup", e => {
-      if (e.key === "Escape") {
-        parent.classList.remove("active");
-      }
-    });
+  selectionFilters.forEach(item => {
+    const buttons = Array.from(item.getElementsByTagName("button"));
 
-    button.addEventListener("keyup", e => {
-      if (e.key === "Enter") {
-        parent.classList.add("active");
-      }
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const isActive = Array.from(button.classList).includes("active");
+        const currentSelections = item
+          .getAttribute("data-selections")
+          .split(",");
+        const val = button.getAttribute("data-value");
+        console.log({ currentSelections });
+
+        if (isActive) {
+          button.classList.remove("active");
+          const valIndex = currentSelections.findIndex(o => o === val);
+          currentSelections.splice(valIndex);
+        } else {
+          button.classList.add("active");
+          currentSelections.push(val);
+        }
+
+        item.setAttribute("data-selections", currentSelections.join(","));
+      });
     });
   });
 })();
